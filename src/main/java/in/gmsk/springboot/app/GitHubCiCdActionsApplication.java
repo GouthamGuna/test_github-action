c5e9ca529b0543da049ced4dd5cabde087bcec29
GouthamGuna <ggowthamsankar@gmail.com>
2022-09-29 21:51:25 +0530

    commit : Java List

--- src/main/java/in/gmsk/springboot/app/GitHubCiCdActionsApplication.java
+++ src/main/java/in/gmsk/springboot/app/GitHubCiCdActionsApplication.java
@@ -1,7 +1,11 @@
 package in.gmsk.springboot.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +21,21 @@ public class GitHubCiCdActionsApplication {
 	@RequestMapping("/test")
 	public String showHey() {
 		return "Hello World Docker with 2nd week...! by GMSK :-) ";
+	}
+	
+	
+	@RequestMapping("/{name}")
+	public List<String> getFriends(@PathVariable ("name") String name){
+		
+		List<String> friend=new ArrayList<>();
+		
+		friend.add("S.ManiKandan");
+		friend.add("Abudal");
+		friend.add("Dinesh");
+		friend.add("Jay Kumar");
+		friend.add("manoj");
+		
+		return friend;
 	}
 
 	public static void main(String[] args) {
