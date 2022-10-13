0fd47a0165c48c180d6a79abaefef1bb6cca92d8
GouthamGuna <ggowthamsankar@gmail.com>
2022-10-13 21:23:43 +0530

    fix - jpa

--- src/main/java/in/gmsk/springboot/app/GitHubCiCdActionsApplication.java
+++ src/main/java/in/gmsk/springboot/app/GitHubCiCdActionsApplication.java
@@ -6,13 +6,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableScheduling
+@RestController
 public class GitHubCiCdActionsApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GitHubCiCdActionsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String sayHelloWorld() {
+		return "Hello World ....";
 	}
 	
 	@Scheduled(fixedDelay = 50000)
